9266d116fe1f7b1d40ae54b95207ca7ecc7ca44e
niklas.sankell <niklas.sankell@gmail.com>
2024-01-07 14:44:56 +0100

    Methods, components and configuration for fetching data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,56 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useEffect, useState } from 'react';
+import { Configuration, ConfigurationParameters, GreetingsApi, DefaultConfig } from './generated';
+import Content from './app/components/Content';
 import './App.css';
 
+const newConfiguration: ConfigurationParameters = {
+  basePath: process.env.API_BASE_URL || "http://localhost:8080",
+};
+DefaultConfig.config = new Configuration(newConfiguration);
+const greetingsApi = new GreetingsApi();
+
 function App() {
+  const [content, setContent] = useState(<></>);
+
+  async function fetchGreetings(): Promise<void> {
+    try {
+      const greetings = await greetingsApi.getGreetings();
+      setContent(<Content greetings={greetings} />)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  useEffect((): void => {
+  }, [content]);
+
+  useEffect((): void => {
+    fetchGreetings();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <main>
+        {content}
+      </main>
     </div>
   );
 }
---
+++ react-app/src/app/components/Content.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Greetings } from '../../generated';
+
+type Props = {
+    greetings: Greetings;
+}
+
+const Content = (props: Props) => {
+    const greetings = props.greetings.greetings === undefined ? [] : props.greetings.greetings;
+    return (<div className="content">
+    <ul>
+        { greetings.map((greeting, index) => (
+        <li key={index} className='greeting-li'>
+            <div className="greeting-wrapper">
+                <span className='greeting'>{greeting.message}</span>
+            </div>
+        </li>) ) }
+    </ul>
+    </div>);
+}
+
+export default Content;

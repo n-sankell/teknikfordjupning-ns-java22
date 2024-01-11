74da1bf7fc6d9211b9d75d34aefbd92e7b7de3a2
niklas.sankell <niklas.sankell@gmail.com>
2024-01-11 09:57:53 +0100

    modifications to dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -30,4 +30,6 @@ COPY --from=builder /app/build ./build
 
 RUN npm install -g serve
 
+EXPOSE 3000
+
 CMD serve -s build -l 3000
--- k8s/load-images.sh
+++ k8s/load-images.sh
@@ -4,7 +4,7 @@
 #minikube image load xtf-postgres
 #echo "DONE"
 echo "minikube image load xtf-react-app"
-minikube image load xtf-react-app
+minikube image load xtf-react-app:k8s
 echo "DONE"
 echo "minikube image load xtf-spring-boot-app:mysql"
 minikube image load xtf-spring-boot-app:mysql
